c567bef1cb37f93f02f1af2fdfece441166945fe
SsaSa6 <zxcvandandand1@Gmail.com>
2026-07-30 23:45:14 +0900

    모든 데이터를 한번에 적재 함 (DB 적재 성공)

--- ELT/load.py
+++ ELT/load.py
@@ -1,17 +1,47 @@
 import db
+import copy
 
 def load(data,collected_at,slot_at):
     conn = db.get_connection()
 
+    change_dict = {"change": "changer", "timestamp": "time_stamp"}
+
+    data_copy = copy.deepcopy(data)
+
     with conn:
         with conn.cursor() as cursor:
-            data[0]["slot_at"] = slot_at
-            data[0]["collected_at"] = collected_at
-            
-            insert_sql = f"INSERT INTO data_set (market,slot_at,collected_at) VALUES (%s,%s,%s)"
-            values = (data[0]["market"],slot_at,collected_at)
+            data_copy[0]["slot_at"] = slot_at
+            data_copy[0]["collected_at"] = collected_at
 
-            cursor.execute(insert_sql,values)
+            for key,items in change_dict.items():
+                 data_copy[0][f"{items}"] = data_copy[0][f"{key}"]
+
+            del data_copy[0]["change"]
+            del data_copy[0]["timestamp"]
+            
+            test = []
+            columns = []
+
+            for key in data_copy[0]:
+                    columns.append(key)
+                    test.append(data_copy[0][key])
+
+            columns = ','.join(columns)
+
+            values = test
+
+            values_sum = []
+
+            for i in range(0,len(data_copy[0])):
+                 values_sum.append("%s")
+
+            values_sum = ','.join(values_sum)
+
+            insert_sql = f"INSERT INTO data_set ({columns}) VALUES ({values_sum})"
+
+            print(data_copy)
+
+            cursor.execute(insert_sql,values) #sql 실행
 
             conn.commit()
 
