e82f6277d4e83649e40adc73a681b60c6be0114b
SsaSa6 <zxcvandandand1@Gmail.com>
2026-08-11 23:49:20 +0900

    extract, load, db dag 추가

--- dags/db_dag.py
+++ dags/db_dag.py
@@ -1,11 +1,7 @@
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
-
-
-def print_hello():
-    print("Hello from PythonOperator")
-
+import db
 
 with DAG(dag_id="sample_dag", start_date=datetime(2026, 8, 6), schedule='@daily') as dag:
-    python_task = PythonOperator(task_id="print_hello", python_callable=print_hello)
+    python_task = PythonOperator(task_id="print_hello", python_callable=get_connection)
