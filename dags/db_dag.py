fc397338e099573aee1119a5059948122dfe57f3
SsaSa6 <zxcvandandand1@Gmail.com>
2026-08-12 23:44:16 +0900

    dag 수정

--- dags/db_dag.py
+++ dags/db_dag.py
@@ -1,7 +1,24 @@
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
-import db
+from airflow.sdk.execution_time.xcom import XCom
+import extract
+import load
 
-with DAG(dag_id="sample_dag", start_date=datetime(2026, 8, 6), schedule='@daily') as dag:
-    python_task = PythonOperator(task_id="print_hello", python_callable=get_connection)
+with DAG(dag_id="Market_ELT", 
+         start_date=datetime(2026, 8, 6), 
+         schedule='@daily'
+    ) as dag:
+
+    def push_xcom(ti): #airflow의 구동 메타 데이터
+        ti.xcom_push(key = "xcom_key", value = extract.extract())
+
+    def pull_xcom(ti):
+        data = ti.xcom_pull(key ="xcom_key", task_ids = "extract_task")
+        load.load(data)
+
+    extract_task = PythonOperator(task_id="extract", python_callable=push_xcom)
+
+    load_task = PythonOperator(task_id="load", python_callable=pull_xcom)
+
+    extract_task >> load_task
--- dags/load_dag.py
+++
@@ -1,7 +0,0 @@
-from airflow.sdk import DAG
-from airflow.providers.standard.operators.python import PythonOperator
-from datetime import datetime
-import load
-
-with DAG(dag_id="sample_dag", start_date=datetime(2026, 8, 6), schedule='@daily') as dag:
-    python_task = PythonOperator(task_id="print_hello", python_callable=load)
