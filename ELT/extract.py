d632ea5d1c6f5fc518b6c915d03c6663fd4d7232
SsaSa6 <zxcvandandand1@Gmail.com>
2026-08-19 23:59:24 +0900

    http 상태 코드 추가 및 sql 오류 수정

--- ELT/extract.py
+++ ELT/extract.py
@@ -8,6 +8,11 @@ def extract():
 
     collected_at = datetime.now()
 
+    if response.status_code != 200:
+        print("요청 실패")
+    else:
+        print("요청 성공")
+
     data = response.json()
 
     return data,collected_at
